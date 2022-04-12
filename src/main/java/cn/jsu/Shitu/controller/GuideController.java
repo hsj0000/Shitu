00172d308b5b3aff782cd6d1b55f1ede4d68289e
Aurora <81908178+hsj0000@users.noreply.github.com>
2022-04-12 14:36:21 +0800

    Update GuideController.java

--- src/main/java/cn/jsu/Shitu/controller/GuideController.java
+++ src/main/java/cn/jsu/Shitu/controller/GuideController.java
@@ -16,66 +16,33 @@ public class GuideController {
     @Autowired
     GuideService GuideService;
 
-    @PostMapping("/signIn")
-    @ApiOperation("用户签到")
-    public CommonResult userSignIn(@RequestBody UserSignInfoVo userSignInfoVo) {
-        return attendanceService.signIn(userSignInfoVo);
+    @PostMapping("/siteToRetrieve")
+    @ApiOperation("地点检索")
+    public CommonResult siteToRetrieve(@RequestBody SiteInfoVo siteInfoVo) {
+        return GuideService.siteToRetrieve(siteInfoVo);
     }
-
-    @PostMapping("/signOut")
-    @ApiOperation("用户签退")
-    public CommonResult userSignOut(@RequestBody UserSignInfoVo userSignInfoVo) {
-        return attendanceService.userSignOut(userSignInfoVo);
+    
+    @PostMapping("/coordinateTransformation")
+    @ApiOperation("坐标转换")
+    public CommonResult coordinateTransformation(@RequestBody CoordinateInfoVo coordinateInfoVo) {
+        return GuideService.coordinateTransformation(coordinateInfoVo);
     }
-
-    @PostMapping("/queryOneDayUserSignData")
-    @ApiOperation("查询一天用户具体签到数据")
-    public CommonResult queryOneDayUserSignData(@RequestBody UserSignInfoVo userSignInfoVo) {
-        return attendanceService.queryOneDayUserSignData(userSignInfoVo);
+    
+    @PostMapping("/trajectoryAnalysis")
+    @ApiOperation("轨迹分析")
+    public CommonResult trajectoryAnalysis(@RequestBody TrajectoryInfoVo trajectoryInfoVo) {
+        return GuideService.trajectoryAnalysis(trajectoryInfoVo);
     }
-
-    @PostMapping("/updateAbsentCause")
-    @ApiOperation("修改缺勤原因")
-    public CommonResult updateAbsentCause(@RequestBody UserSignInfoVo userSignInfoVo) {
-        return attendanceService.updateAbsentCause(userSignInfoVo);
+    
+    @PostMapping("/BatchCalculateWay")
+    @ApiOperation("批量算路")
+    public CommonResult BatchCalculateWay(@RequestBody WayInfoVo wayInfoVo) {
+        return GuideService.BatchCalculateWay(wayInfoVo);
     }
-
-    @PostMapping("/queryEachDaySignSituation")
-    @ApiOperation("查询一天所有人签到情况")
-    public CommonResult queryEachDaySignSituation(@RequestBody UserSignInfoVo userSignInfoVo) {
-        return attendanceService.queryEachDaySignSituation(userSignInfoVo);
-    }
-
-    @PostMapping("/queryTotalSignTimeByQuantum")
-    @ApiOperation("查询不同时间段用户签到时长")
-    public CommonResult queryTotalSignTimeByQuantum(@RequestBody QuantumVo quantumVo) {
-        return attendanceService.queryTotalSignTimeByQuantum(quantumVo);
-    }
-
-    @PostMapping("/queryAbsenceTimeByQuantum")
-    @ApiOperation("查询不同时间段用户缺勤天数")
-    public CommonResult queryAbsenceTimeByQuantum(@RequestBody QuantumVo quantumVo) {
-        return attendanceService.queryAbsenceTimeByQuantum(quantumVo);
-    }
-
-//    @PostMapping("/queryQuantumUser")
-//    @ApiOperation("查询一段时间用户具体签到数据表")
-//    public CommonResult queryQuantumUser(@RequestBody UserSignInfoVo userSignInfoVo) {
-//        return attendanceService.queryQuantumUser(userSignInfoVo);
-//    }
-
-
-    //不用->
-//-----------------------------------------------------------------------------------------------
-    @PostMapping("/yearSignOutTable")
-    @ApiOperation("年度签到表")
-    public CommonResult yearSignOutTable(@RequestBody UserSignInfoVo userSignInfoVo) {
-        return attendanceService.yearSignOutTable(userSignInfoVo);
-    }
-
-    @PostMapping("/queryOneDaySingleSignSituation")
-    @ApiOperation("查询一天用户签到情况")
-    public CommonResult queryOneDaySingleSignSituation(@RequestBody UserSignInfoVo userSignInfoVo) {
-        return attendanceService.queryOneDaySingleSignSituation(userSignInfoVo);
+    
+    @PostMapping("/realTimeTrafficQuery")
+    @ApiOperation("实时路况查询")
+    public CommonResult realTimeTrafficQuery(@RequestBody TrafficInfoVo trafficInfoVo) {
+        return GuideService.realTimeTrafficQuery(trafficInfoVo);
     }
 }
